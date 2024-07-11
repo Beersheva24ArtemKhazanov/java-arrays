356cc91181fea7c6514ac0c5aad426f8b7572ce1
Artem Khazanov <tulenka04@gmail.com>
2024-07-11 08:20:14 +0300

    create branch differ and added different solutions of mothods insert and remove

--- src/main/java/telran/util/Arrays.java
+++ src/main/java/telran/util/Arrays.java
@@ -17,48 +17,34 @@ public class Arrays {
         return res;
     }
 
-    // public static int[] insert(int[] ar, int index, int number) {
-    //     // TODO
-    //     int[] res = java.util.Arrays.copyOf(ar, ar.length + 1);
-    //     int key = 0;
-    //     while (key < index) {
-    //         res[key] = ar[key];
-    //         key++;
-    //     }
-    //     res[index] = number;
-    //     while (key < ar.length) {
-    //         res[key + 1] = ar[key];
-    //         key++;
-    //     }
-    //     return res;
-    // }
-
-    // public static int[] remove(int[] numbers, int index) {
-    //     // TODO
-    //     int[] res = java.util.Arrays.copyOf(numbers, numbers.length - 1);
-    //     int key = 0;
-    //     while (key <= index) {
-    //         res[key] = numbers[key];
-    //         key++;
-    //     }
-    //     while (key > index && key -1  < res.length) {
-    //         res[key - 1] = numbers[key];
-    //         key++;
-    //     }
-    //     return res;
-    // }
-
     public static int[] insert(int[] ar, int index, int number) {
+        // TODO
         int[] res = java.util.Arrays.copyOf(ar, ar.length + 1);
-        System.arraycopy(ar, 0, res, 0, index);
+        int key = 0;
+        while (key < index) {
+            res[key] = ar[key];
+            key++;
+        }
         res[index] = number;
-        System.arraycopy(ar, index, res, index + 1, ar.length - index);
+        while (key < ar.length) {
+            res[key + 1] = ar[key];
+            key++;
+        }
         return res;
     }
 
     public static int[] remove(int[] numbers, int index) {
+        // TODO
         int[] res = java.util.Arrays.copyOf(numbers, numbers.length - 1);
-        System.arraycopy(numbers, index + 1, res, index, numbers.length - index - 1);
+        int key = 0;
+        while (key <= index) {
+            res[key] = numbers[key];
+            key++;
+        }
+        while (key > index && key -1  < res.length) {
+            res[key - 1] = numbers[key];
+            key++;
+        }
         return res;
     }
 }
